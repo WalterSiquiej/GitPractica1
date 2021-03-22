b926947c25b216dced258fe1036af28cf8c1d070
walter <walter@Pc-WalterSiquiej>
2021-03-21 23:07:44 -0600

    Version actualizada 1

--- src/Paquete1/Menu.java
+++ src/Paquete1/Menu.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Paquete1;
+
+import java.util.Scanner;
 
 /**
  *
  * @author walter
  */
 public class Menu {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+        Scanner entrada=new Scanner (System.in);      
+        int numero;
+        boolean salir=false;
+        
+        while (salir != true)
+        {
+            System.out.println( "---Ecosistema de Dispositivos"+"---\n"+
+                    "1. Crear dispositivo\n"+
+                    "2. Administrar dispositivos\n"+
+                    "3. Acciones con dispositivos\n"+
+                    "4. Acciones externas de dispositivos\n"+
+                    "5. Cargas masivas\n"+
+                    "6. Logs\n"+
+                    "0. Salir\n"+
+                     "Seleccione la opcion a ejecutar");
+           numero = entrada.nextInt();
+                   
+           switch (numero)
+           {
+               case 1:
+                   CrearDispositivos creardispositivos = new CrearDispositivos ();
+                   
+                   break;
+               case 2:
+                   AdministrarDispositivos administrardispositivos = new AdministrarDispositivos ();
+                   break;
+               case 3:
+                   AccionesConDispositivos accionescondispositivos = new AccionesConDispositivos ();
+                   break;
+               case 4:
+                   AccionesExternasdeDispositivos accionesexternasdedispositivos = new AccionesExternasdeDispositivos ();
+                   break;
+               case 5:
+                   CargasMasivas accionesmasivas = new CargasMasivas ();
+                   break;
+               case 6:
+                   Logs logs = new Logs();
+                   break;
+               case 0:
+                   salir=true;
+                   break;
+            }
+                    
+                    
+        }
+        
+     
     }
+    
     
 }
